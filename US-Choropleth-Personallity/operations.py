bc57df97ac7502cf6fd1d66983e2fe02351a9013
tapa8728 <tapa8728@colorado.edu>
2016-01-05 23:13:57 -0700

    Spaces between values cleaned up

--- US-Choropleth-Personallity/operations.py
+++ US-Choropleth-Personallity/operations.py
@@ -1,5 +1,18 @@
 #operations on the personality data
 
+# Raw data in comma-separated format
 f = open("data.txt","r") 	#opens file with name of "data.txt"
-print(f.read(1))
-print(f.read())
+lines = f.readlines()
+
+# Step 1: Clean up data by removing spaces between each value
+print "lets clean up the data"
+for l in lines:
+	print l
+	new_line = str.replace(str(l),' ','')
+	print new_line
+
+print "File after replacing spaces"
+
+	
+
+#Step 2: Apply formula for O and dump the data in a new file "data_1.txt"

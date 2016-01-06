252dd40e328697834cadf4b98674701fcc0922a4
tapa8728 <tapa8728@colorado.edu>
2016-01-06 09:19:54 -0700

    Value of O calculated. List of lists created.

--- US-Choropleth-Personallity/operations.py
+++ US-Choropleth-Personallity/operations.py
@@ -5,14 +5,16 @@ f = open("data.txt","r") 	#opens file with name of "data.txt"
 lines = f.readlines()
 
 # Step 1: Clean up data by removing spaces between each value
-print "lets clean up the data"
 for l in lines:
-	print l
-	new_line = str.replace(str(l),' ','')
-	print new_line
 
-print "File after replacing spaces"
+	tmp1=l.replace(" ","").replace("\n","").split(",")
+	try:
+		tmp2=int(tmp1[2])+int(tmp1[3])+int(tmp1[4])
+	except:
+		print "Cnnot be converted into integer"
+	tmp1.extend([tmp2])
+	print tmp1
 
-	
+#1001, NV, 002, 36,45,81, 52	
 
 #Step 2: Apply formula for O and dump the data in a new file "data_1.txt"
